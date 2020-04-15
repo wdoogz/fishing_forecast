1509cfc9e3a387ed1e3e4ccc9e178961286b18af
doogz <wdugan7@gmail.com>
2020-04-15 04:15:03 +0100

    update

--- gather.py
+++ gather.py
@@ -15,9 +15,10 @@ def zip_to_geo(zipcode):
             os.makedirs("/tmp/geocodes")
         except Exception as e:
             pass
-        with open("/tmp/apikey") as ak:
-            URL = "https://www.zipcodeapi.com/rest/{}/info.json/{}/degrees".format(ak.read(),zipcode)
-        
+        with open("/tmp/apikey", "r") as ak:
+            key = ak.read().split()[0]
+            URL = "https://www.zipcodeapi.com/rest/{}/info.json/{}/degrees".format(key,zipcode)
+            print(URL) 
         geo_co = requests.get(URL).json()
         
         city = geo_co["city"]
@@ -29,15 +30,14 @@ def zip_to_geo(zipcode):
 
         with open(locofile, "w+") as f:
             f.write(jsontest)
-            json_out = f.read()
-            print(json_out)
+        json_out = json.loads(open(locofile,"r").read())
 
     return json_out
 
 def get_forecast(geoloco):
     todaydate = datetime.datetime.now()
     date = todaydate.strftime("%Y%m%d")
-    URL = "https://api.solunar.org/solunar/{},{},{},-4".format(str(geoloco["lat"]), str(geoloco["lng"]), date)
+    URL = "https://api.solunar.org/solunar/{},{},{},-4".format(str(geoloco["lat"]), str(geoloco["lng"]), str(date))
     solunar_req = requests.get(URL).json()
     hourly_rating = solunar_req["hourlyRating"]
     print(json.dumps(hourly_rating, indent=4))
@@ -51,4 +51,4 @@ def get_forecast(geoloco):
                 print(k, "BAD Don't fish")
 
 
-get_forecast(zip_to_geo(17856))
+get_forecast(zip_to_geo(14094))
